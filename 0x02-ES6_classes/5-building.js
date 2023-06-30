60cfa93d99ccdb2c25a017800e7883c5ebc71ee2
MjKamma <Mercyigwe1995@gmail.com>
2023-06-30 01:01:01 +0000

    update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,21 @@
 export default class Building {
-  constructor(sqft = 0) {
+  constructor(sqft) {
     if (
-      this.constructor !== Building &&
-      typeof this.evacuationWarningMessage !== 'function'
+      this.constructor !== Building
+      && typeof this.evacuationWarningMesagge !== 'function'
     ) {
       throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
+        'Class extending Building must override evacuationWarningMessage',
       );
     }
-
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
