d1dab051cb99cf5aa58deed2ec26eaa97aaf2867
MjKamma <Mercyigwe1995@gmail.com>
2023-07-05 00:19:38 +0000

    0x03-ES6_data_manipulation tasks

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,24 @@
+export default function updateStudentGradeByCity(
+  getListStudents,
+  city,
+  newGrades
+) {
+  const studentsCity = getListStudents.filter(
+    (student) => student.location === city
+  );
+  return studentsCity.map((student) => {
+    const filteredGrades = newGrades.filter(
+      (grade) => grade.studentId === student.id
+    );
+    if (filteredGrades.length > 0) {
+      return {
+        ...student,
+        grade: filteredGrades[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
+  });
+}
